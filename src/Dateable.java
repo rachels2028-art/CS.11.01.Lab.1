0f05778e5beb14a8b380c3bf64225e36fb48cd60
rachels2028-art <rachels2028@student.cis.edu.hk>
2025-09-16 15:03:32 +0800

    done lab 1.2

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Dateable {
 
-    public static void main(String[] args) {
-        
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        //char c = ‘A’;  Int n = 14;
+        System.out.print("Enter your age");
+        int yourage = sc.nextInt();
+        int age = 7+ (yourage/2);
+        System.out.println( yourage + "-year olds should date somebody that is at least " + age);
+
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,16 @@
+import java.util.Scanner;
+
 public class Pages {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        //char c = ‘A’;  Int n = 14;
+        System.out.print("Enter your age: ");
+        int myAge = sc.nextInt();
+        int age1 = 100-myAge ;
+        System.out.println( myAge + "-year olds should read at least "
+                + age1 + " before giving up the book");
 
     }
 }
